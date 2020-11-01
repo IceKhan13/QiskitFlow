efb34a8dc027a33aacacf7ef49be49fee87fae91
IceKhan13 <iskandar.sitdikov@gmail.com>
2020-10-31 21:05:52 -0700

    UI: experiments page layout

--- ui/app/containers/App/index.js
+++ ui/app/containers/App/index.js
@@ -6,20 +6,35 @@
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb } from 'antd';
 
-import HomePage from 'containers/HomePage/Loadable';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import ExperimentsList from '../ExperimentsList/Loadable';
 import ExperimentRunsList from '../ExperimentRunsList/Loadable';
 import Run from '../Run/Loadable';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const logoStyle = {
+  height: '32px',
+  margin: '16px',
+  background: 'rgba(255, 255, 255, 0.3)',
+};
 
 export default function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div>
       <Helmet
@@ -31,40 +46,48 @@ export default function App() {
           content="QiskitFlow. Reproducible quantum experiments."
         />
       </Helmet>
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['experiments']}>
-            <Menu.Item key="home">Home</Menu.Item>
-            <Menu.Item key="experiments">Experiments</Menu.Item>
-            <Menu.Item key="profile">Profile</Menu.Item>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+          <div style={logoStyle} />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="home" icon={<PieChartOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="experiments" icon={<DesktopOutlined />}>
+              Experiments
+            </Menu.Item>
+            <SubMenu key="profile" icon={<UserOutlined />} title="Profile">
+              <Menu.Item key="profile">Profile</Menu.Item>
+              <Menu.Item key="logout">Logout</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
           </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            style={{
-              height: '85vh',
-              backgroundColor: 'white',
-              padding: '16px',
-            }}
-          >
-            <Switch>
-              <Route exact path="/" component={ExperimentsList} />
-              <Route path="/experiments" component={ExperimentsList} />
-              <Route path="/experiments/:id" component={ExperimentRunsList} />
-              <Route path="/run/:id" component={Run} />
-              <Route path="" component={NotFoundPage} />
-            </Switch>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }} >
-          QiskitFlow ©2020
-        </Footer>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360, background: '#fff' }}
+            >
+              <Switch>
+                <Route exact path="/" component={ExperimentsList} />
+                <Route exact path="/experiments" component={ExperimentsList} />
+                <Route path="/experiments/:experimentId" component={ExperimentRunsList} />
+                <Route path="/runs/:id" component={Run} />
+                <Route path="" component={NotFoundPage} />
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>QiskitFlow ©2020</Footer>
+        </Layout>
       </Layout>
     </div>
   );
--- ui/app/containers/ExperimentsList/index.js
+++ ui/app/containers/ExperimentsList/index.js
@@ -4,37 +4,189 @@
  *
  */
 
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
+import { Table, Button, Tag } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
+import { Link } from 'react-router-dom';
 import makeSelectExperimentsList from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
+
+const expandedRowRender = record => {
+  const columns = [
+    {
+      title: 'Run id',
+      dataIndex: 'uuid',
+      key: 'uuid',
+      render: uuid => <Link to={`/runs/${uuid}`}>{`Run [${uuid}]`}</Link>,
+    },
+    {
+      title: 'Metrics',
+      dataIndex: 'metrics',
+      key: 'metrics',
+      render: metrics => {
+        const metricsList = metrics.map((m, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={`metric_${m.name}_${i}`}>{`${m.name}: ${m.value}`}</div>
+        ));
+        return <span>{metricsList}</span>;
+      },
+    },
+    {
+      title: 'Parameters',
+      dataIndex: 'parameters',
+      key: 'parameters',
+      render: parameters => {
+        const parametersList = parameters.map((p, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={`param_${p.name}_${i}`}>{`${p.name}: ${p.value}`}</div>
+        ));
+        return <span>{parametersList}</span>;
+      },
+    },
+    {
+      title: '# measurements',
+      dataIndex: 'measurements',
+      key: 'measurements',
+      render: measurements => measurements.length,
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      render: () => (
+        <span>
+          <Button danger size="small" type="text" icon={<DeleteOutlined />}>
+            Delete
+          </Button>
+        </span>
+      ),
+    },
+  ];
+
+  return (
+    <Table columns={columns} dataSource={record.runs} pagination={false} />
+  );
+};
+
+const data = [...Array(20).keys()].map(idx => ({
+    key: idx,
+    id: idx,
+    createdAt: '2014-12-24 23:12:00',
+    name: 'This is production name',
+    tags: ['Qiskit', 'Teleportation'],
+    version: '0.0.1',
+    author: 'Admin',
+    n_runs: 10,
+    runs: [
+      {
+        uuid: '123123123asdasdasd',
+        name: 'Name',
+        metrics: [
+          {
+            name: 'runtime',
+            value: 10000,
+          },
+          {
+            name: 'somename',
+            value: 100,
+          },
+        ],
+        parameters: [
+          {
+            name: 'backend',
+            value: 'Tashkent',
+          },
+        ],
+        measurements: [],
+        entrypoint: null,
+      },
+      {
+        uuid: '123123123asdasdasd123',
+        name: 'Name',
+        metrics: [
+          {
+            name: 'runtime',
+            value: 10000,
+          },
+          {
+            name: 'somename',
+            value: 100,
+          },
+        ],
+        parameters: [
+          {
+            name: 'backend',
+            value: 'Tashkent',
+          },
+        ],
+        measurements: [],
+        entrypoint: null,
+      },
+    ],
+  }));
 
 export function ExperimentsList() {
   useInjectReducer({ key: 'experimentsList', reducer });
   useInjectSaga({ key: 'experimentsList', saga });
 
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [experiments, setExperiments] = useState(data);
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (name, row) => <Link to={`/experiments/${row.id}`}>{name}</Link>,
+    },
+    {
+      title: '# of runs',
+      dataIndex: 'runs',
+      key: 'runs',
+      render: runs => runs.length,
+    },
+    { title: 'Version', dataIndex: 'version', key: 'version' },
+    { title: 'Author', dataIndex: 'author', key: 'author' },
+    { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
+    {
+      title: 'Tags',
+      key: 'tags',
+      render: row => {
+        const tagsList = row.tags.map((tag, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Tag key={`tags_${tag}_${i}`}>{tag}</Tag>
+        ));
+        return <div>{tagsList}</div>;
+      },
+    },
+  ];
+
   return (
     <div>
       <Helmet>
-        <title>ExperimentsList</title>
-        <meta name="description" content="Description of ExperimentsList" />
+        <title>Experiments</title>
+        <meta name="description" content="QiskitFlow. Experiments list." />
       </Helmet>
-      <FormattedMessage {...messages.header} />
+      <Table
+        columns={columns}
+        expandable={{ expandedRowRender }}
+        dataSource={experiments}
+      />
     </div>
   );
 }
 
 ExperimentsList.propTypes = {
+  // eslint-disable-next-line react/no-unused-prop-types
   dispatch: PropTypes.func.isRequired,
 };
 
