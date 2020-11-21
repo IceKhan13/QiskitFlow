38d32dcd320cb71a29b7162330f09ee5da8db110
Iskandar Sitdikov <IceKhan13@users.noreply.github.com>
2020-11-20 20:39:34 -0800

    Lib: file extension ignore for sourcecode of experiment (#26)

--- lib/qiskitflow/utils/utils.py
+++ lib/qiskitflow/utils/utils.py
@@ -1,5 +1,37 @@
+import fnmatch
+from os.path import isdir, join
+
 from .constants import EXPERIMENTS_DIRECTORY
 
 
 def get_experiment_folder(name: str) -> str:
     return "{}/{}".format(EXPERIMENTS_DIRECTORY, name)
+
+
+def include_patterns(*patterns):
+    """ Function that can be used as shutil.copytree() ignore parameter that
+    determines which files *not* to ignore, the inverse of "normal" usage.
+
+    This is a factory function that creates a function which can be used as a
+    callable for copytree()'s ignore argument, *not* ignoring files that match
+    any of the glob-style patterns provided.
+
+    ‛patterns’ are a sequence of pattern strings used to identify the files to
+    include when copying the directory tree.
+
+    Example usage:
+
+        copytree(src_directory, dst_directory,
+                 ignore=include_patterns('*.sldasm', '*.sldprt'))
+    """
+    def _ignore_patterns(path, all_names):
+        # Determine names which match one or more patterns (that shouldn't be
+        # ignored).
+        keep = (name for pattern in patterns
+                        for name in fnmatch.filter(all_names, pattern))
+        # Ignore file names which *didn't* match any of the patterns given that
+        # aren't directory names.
+        dir_names = (name for name in all_names if isdir(join(path, name)) and name == EXPERIMENTS_DIRECTORY)
+        return set(all_names) - set(keep) - set(dir_names)
+
+    return _ignore_patterns
