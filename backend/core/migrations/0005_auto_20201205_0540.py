96826d69f773e1570df6b27b2940412eed43a77d
IceKhan13 <iskandar.sitdikov@gmail.com>
2020-12-04 22:25:04 -0800

    Backend: migration atomicity

--- backend/core/migrations/0005_auto_20201205_0540.py
+++ backend/core/migrations/0005_auto_20201205_0540.py
@@ -4,7 +4,8 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
+    atomic = False
+    
     dependencies = [
         ('core', '0004_rename_measurement_entry_field'),
     ]
