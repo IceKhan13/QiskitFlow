9f1ae76d7c60fbc8a396a47305cc5c0d72d6f382
IceKhan13 <iskandar.sitdikov@gmail.com>
2021-02-20 01:23:49 -0800

    Backend: tests fix

--- backend/qiskitflow_backend/settings.py
+++ backend/qiskitflow_backend/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'github_actions',
-        'USER': 'radorado',
-        'PASSWORD': 'radorado',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
