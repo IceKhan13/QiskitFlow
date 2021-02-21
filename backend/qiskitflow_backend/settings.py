e1b6562658dcc4ebfcceec21c95d5439593ab14b
IceKhan13 <iskandar.sitdikov@gmail.com>
2021-02-20 19:20:54 -0800

    Backend: docker-compose

--- backend/qiskitflow_backend/settings.py
+++ backend/qiskitflow_backend/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'qiskitflow_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'github_actions',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.environ.get("DB_NAME", "github_actions"),
+        'USER': os.environ.get("DB_USER", "postgres"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", "postgres"),
+        'HOST': os.environ.get("DB_HOST", '127.0.0.1'),
+        'PORT': os.environ.get("DB_PORT", '5432'),
     }
 }
 
